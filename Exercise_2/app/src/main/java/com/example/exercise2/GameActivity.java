82d3bd9456161389097c08630bd4b0a938fd0ec4
ByAlonso <alonso.rapado@hotmail.com>
2022-02-10 19:16:04 +0100

    finally

--- Exercise_2/app/src/main/java/com/example/exercise2/GameActivity.java
+++ Exercise_2/app/src/main/java/com/example/exercise2/GameActivity.java
@@ -1,4 +1,6 @@
-package com.example.myapplication;
+package com.example.exercise2;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
@@ -6,94 +8,109 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.livelife.motolibrary.Game;
 import com.livelife.motolibrary.GameType;
 import com.livelife.motolibrary.MotoConnection;
 import com.livelife.motolibrary.OnAntEventListener;
 
-public class GameActivity extends AppCompatActivity implements OnAntEventListener {
-
+public class GameActivity extends AppCompatActivity implements OnAntEventListener
+{
     MotoConnection connection = MotoConnection.getInstance();
-    GameName game_object = new GameName();
+    ColourRace game_object = new ColourRace(); // Game object
     LinearLayout gt_container;
 
     //Stop the game when we exit activity
     @Override
-    public void onBackPressed()
-    {
+    public void onBackPressed() {
         super.onBackPressed();
         game_object.stopGame();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_layout);
+        setContentView(R.layout.activity_game);
+
         connection.registerListener(this);
         connection.setAllTilesToInit();
+
         gt_container = findViewById(R.id.game_type_container);
 
-        for (final GameType gt : game_object.getGameTypes()) {
+        for (final GameType gt : game_object.getGameTypes())
+        {
             Button b = new Button(this);
             b.setText(gt.getName());
-            b.setOnClickListener(new View.OnClickListener() {
+            b.setOnClickListener(new View.OnClickListener()
+            {
                 @Override
-                public void onClick(View v) {
+                public void onClick(View v)
+                {
                     game_object.selectedGameType = gt;
                     game_object.startGame();
                 }
             });
             gt_container.addView(b);
         }
-        game_object.setOnGameEventListener(new Game.OnGameEventListener() {
 
-            final TextView scoreValue = findViewById(R.id.scoreValue);
-            final TextView timeValue = findViewById(R.id.timeValue);
-
+        game_object.setOnGameEventListener(new Game.OnGameEventListener()
+        {
+            TextView scoreValue = findViewById(R.id.scoreValue);
+            TextView timeValue = findViewById(R.id.timeValue);
             @Override
-            public void onGameTimerEvent(int i) {
+
+            public void onGameTimerEvent(int i)
+            {
                 timeValue.setText("Time: " + String.valueOf(i));
             }
 
             @Override
-            public void onGameScoreEvent(int i, int i1) {
+            public void onGameScoreEvent(int i, int i1)
+            {
                 scoreValue.setText(String.valueOf(i));
             }
 
             @Override
-            public void onGameStopEvent() {
-                //this.scoreValue.setText("Stop");
-            }
-
-            @Override
-            public void onSetupMessage(String s) {
-            }
-
-            @Override
-            public void onGameMessage(String s) {
+            public void onGameStopEvent()
+            {
 
             }
 
             @Override
-            public void onSetupEnd() {
+            public void onSetupMessage(String s)
+            {
+
             }
 
+            @Override
+            public void onGameMessage(String s)
+            {
+
+            }
+
+            @Override
+            public void onSetupEnd()
+            {
+
+            }
         });
     }
+
     @Override
-    public void onMessageReceived(byte[] bytes, long l) {
+    public void onMessageReceived(byte[] bytes, long l)
+    {
         game_object.addEvent(bytes);
     }
 
     @Override
-    public void onAntServiceConnected() {
+    public void onAntServiceConnected()
+    {
 
     }
 
     @Override
-    public void onNumbersOfTilesConnected(int i) {
+    public void onNumbersOfTilesConnected(final int i)
+    {
 
     }
 }
