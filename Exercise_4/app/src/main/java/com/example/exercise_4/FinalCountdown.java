e01316cf12567f377b383151ba22ddee8a4253a8
Timothy Yih <timmyyih@gmail.com>
2022-02-24 15:10:44 +0100

    Added Exercise 4

---
+++ Exercise_4/app/src/main/java/com/example/exercise_4/FinalCountdown.java
@@ -0,0 +1,76 @@
+package com.example.exercise_4;
+
+import static com.livelife.motolibrary.AntData.LED_COLOR_OFF;
+import static com.livelife.motolibrary.AntData.LED_COLOR_RED;
+
+import com.livelife.motolibrary.AntData;
+import com.livelife.motolibrary.Game;
+import com.livelife.motolibrary.GameType;
+import com.livelife.motolibrary.MotoConnection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FinalCountdown extends Game {
+    MotoConnection connection = MotoConnection.getInstance();
+    ArrayList<Integer> colorList = AntData.allColors();
+
+
+    FinalCountdown(){
+        setName("Final Countdown");
+
+        GameType gt = new GameType(1, GameType.GAME_TYPE_TIME, 30, "1 player 30 sec",1);
+        addGameType(gt);
+
+        GameType gt2 = new GameType(2, GameType.GAME_TYPE_TIME, 60, "1 player 1 min",1);
+        addGameType(gt2);
+
+        GameType gt3 = new GameType(3, GameType.GAME_TYPE_TIME, 60*2, "1 player 2 min",1);
+        addGameType(gt3);
+    }
+
+    @Override
+    public void onGameStart()
+    {
+        super.onGameStart();
+        clearPlayersScore();
+        updateTiles();
+    }
+
+    @Override
+    public void onGameUpdate(byte[] message)
+    {
+        super.onGameUpdate(message);
+
+        int event = AntData.getCommand(message);
+        int color = AntData.getColorFromPress(message);
+
+        if (event == AntData.EVENT_PRESS && color==colorList.get(0)){
+
+            incrementPlayerScore(1,0);
+        }
+        else {
+            incrementPlayerScore(-2,0);
+        }
+        updateTiles();
+    }
+
+    // Some animation on the tiles once the game is over
+    @Override
+    public void onGameEnd()
+    {
+        super.onGameEnd();
+        connection.setAllTilesBlink(4,LED_COLOR_RED);
+
+    }
+
+    public void updateTiles(){
+        connection.setAllTilesIdle(LED_COLOR_OFF);
+        int randomTile = connection.randomIdleTile();
+
+        Collections.shuffle(colorList);
+
+        connection.setAllTilesColor(colorList.get(1));
+        connection.setTileColor( colorList.get(0) , randomTile);
+    }
+}
