f5748933378f776abf7845071d6f08d21bdbe1a9
A01748293 <A01748293@itesm.mx>
2022-03-10 16:36:51 +0100

    Exercise 6 halfway

---
+++ Exercise_6_group_3/app/src/main/java/com/example/exercise_5_solution/ColourRace.java
@@ -0,0 +1,77 @@
+package com.example.exercise_5_solution;
+
+import static com.livelife.motolibrary.AntData.LED_COLOR_BLUE;
+import static com.livelife.motolibrary.AntData.LED_COLOR_OFF;
+import static com.livelife.motolibrary.AntData.LED_COLOR_RED;
+
+import android.util.Log;
+
+import com.livelife.motolibrary.AntData;
+import com.livelife.motolibrary.Game;
+import com.livelife.motolibrary.GameType;
+import com.livelife.motolibrary.MotoConnection;
+
+public class ColourRace extends Game
+{
+    MotoConnection connection = MotoConnection.getInstance();
+
+    ColourRace()
+    {
+        setName("Colour Race");
+
+        GameType gt = new GameType(1, GameType.GAME_TYPE_TIME, 30, "1 player 30 sec",1);
+        addGameType(gt);
+
+        GameType gt2 = new GameType(2, GameType.GAME_TYPE_TIME, 60, "1 player 1 min",1);
+        addGameType(gt2);
+
+        GameType gt3 = new GameType(3, GameType.GAME_TYPE_TIME, 60*2, "1 player 2 min",1);
+        addGameType(gt3);
+    }
+
+    // Some game setup stuff
+    @Override
+    public void onGameStart()
+    {
+        super.onGameStart();
+        connection.setAllTilesIdle(LED_COLOR_OFF);
+        //updateTiles();          //Set off and then turn one blue
+    }
+
+    // Put game logic here
+    @Override
+    public void onGameUpdate(byte[] message)
+    {
+        super.onGameUpdate(message);
+
+        int event = AntData.getCommand(message);
+        int color= AntData.getColorFromPress(message);
+        if (event == AntData.EVENT_PRESS)
+        {
+            if(color!=LED_COLOR_OFF) {
+                incrementPlayerScore(1,0);
+                this.getOnGameEventListener().onGameTimerEvent(-200);
+            }
+
+        }else{
+            this.getOnGameEventListener().onGameTimerEvent(0);
+        }
+    }
+
+    // Some animation on the tiles once the game is over
+    @Override
+    public void onGameEnd()
+    {
+        super.onGameEnd();
+
+        connection.setAllTilesBlink(4,LED_COLOR_RED);
+    }
+    public void updateTiles(){
+        int randomTile = connection.randomIdleTile();
+        connection.setAllTilesIdle(LED_COLOR_OFF);
+        connection.setTileColor(LED_COLOR_BLUE, randomTile);
+    }
+
+}
+
+
---
+++ Exercise_6_group_3/app/src/main/java/com/example/exercise_5_solution/GameActivity.java
@@ -0,0 +1,168 @@
+package com.example.exercise_5_solution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.livelife.motolibrary.AntData;
+import com.livelife.motolibrary.Game;
+import com.livelife.motolibrary.GameType;
+import com.livelife.motolibrary.MotoConnection;
+import com.livelife.motolibrary.OnAntEventListener;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameActivity extends AppCompatActivity implements OnAntEventListener
+{
+    MotoConnection connection = MotoConnection.getInstance();
+    //MotoSound sound = MotoSound.getInstance();
+    ColourRace game_object = new ColourRace();
+    LinearLayout gt_container;
+
+    int delay = 4000; // Default time that the user has to press a tile
+
+    int points_scored = 0; // Points scored by the user
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        connection.registerListener(this);
+        connection.setAllTilesToInit();
+
+        gt_container = findViewById(R.id.game_type_container);
+
+        for (final GameType gt : game_object.getGameTypes())
+        {
+            Button b = new Button(this);
+            b.setText(gt.getName());
+            b.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    game_object.selectedGameType = gt;
+                    //sound.playStart();
+                    game_object.startGame();
+                }
+            });
+            gt_container.addView(b);
+        }
+
+        final TextView player_score = findViewById(R.id.score_value); // Where the player's score is displayed
+
+
+        game_object.setOnGameEventListener(new Game.OnGameEventListener()
+        {
+            // Changing the time the user gets to hit the tile
+            @Override
+            public void onGameTimerEvent(int i)
+            {
+                if (i < 0)
+                {
+                    if (delay >= 2000)
+                    {
+                        delay += i;
+                    }
+                }
+                else
+                {
+                    delay += i;
+                }
+            }
+
+            @Override
+            public void onGameScoreEvent(final int i, int i1)
+            {
+                // Updating the score
+                points_scored = i;
+                GameActivity.this.runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        player_score.setText(String.valueOf(points_scored));
+                    }
+                });
+            }
+
+            @Override
+            public void onGameStopEvent()
+            {
+
+            }
+
+            @Override
+            public void onSetupMessage(String s)
+            {
+
+            }
+
+            @Override
+            public void onGameMessage(String s)
+            {
+
+            }
+
+            @Override
+            public void onSetupEnd()
+            {
+
+            }
+        });
+
+        // Displaying each colour for a certain period of time (default - 3000 ms)
+        Thread my_thread = new Thread()
+        {
+
+            String end_message = "Game Over"; // This message is displayed at the end of the last round
+            int round = 1; // Stores the current round number
+
+            @Override
+            public void run()
+            {
+                final Handler h = new Handler();
+
+                final Runnable r = new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        game_object.updateTiles();
+
+                    }
+                };
+                h.postDelayed(r, delay);
+            }
+
+        };
+
+        my_thread.start();
+
+    }
+
+    @Override
+    public void onMessageReceived(byte[] bytes, long l)
+    {
+        game_object.addEvent(bytes);
+    }
+
+    @Override
+    public void onAntServiceConnected()
+    {
+
+    }
+
+    @Override
+    public void onNumbersOfTilesConnected(int i)
+    {
+
+    }
+}
